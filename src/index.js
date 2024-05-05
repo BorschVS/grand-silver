d4059a5eceef6fdc436e41d0d64d741d5f84dcf1
BorschVS <borschvadym@gmail.com>
2024-05-05 22:39:18 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./components/App";
-import { BrowserRouter } from "react-router-dom";
-import "./styles/base.scss";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/base.scss';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/grand-silver">
       <App />
     </BrowserRouter>
   </React.StrictMode>
